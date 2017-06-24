52a6fdbfd492a160f8ddf9b0e71ed63bd422dd0d
Dean Camera <dean@fourwalledcubicle.com>
2017-06-24 13:17:59 +1000

    Add IPv6 blocking.

--- update.sh
+++ update.sh
@@ -9,6 +9,10 @@ if [[ $? != 0 ]] ; then
   # Replace hosts file with the updated list
   mv /etc/hosts.dnsmasq.new /etc/hosts.dnsmasq
 
+  # Convert ipv4 host blocking entries into additional ipv6 blocking entries
+  sort /etc/hosts.dnsmasq.new | uniq | grep "^0" > /etc/hosts_ipv6.dnsmasq
+  sort /etc/hosts.dnsmasq.new | uniq | grep "^0" | sed "s/0\.0\.0\.0/::/g" >> /etc/hosts_ipv6.dnsmasq
+
   # Kill the main process. Docker will bring back up the container when restart=always is set.
   pkill dnsmasq
 fi
