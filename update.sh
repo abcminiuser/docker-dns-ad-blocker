8b5c24a93ab6ee8965868b4ad941ee08581d57a7
Dean Camera <dean@fourwalledcubicle.com>
2017-06-24 13:34:45 +1000

    Better logging when updating blocklist.

--- update.sh
+++ update.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
 
+REMOTE_BLOCKLIST_URL="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+
+BLOCKLIST_TEMP_FILE="/etc/hosts.dnsmasq.new"
+
+BLOCKLIST_TARGET_FILE="/etc/hosts.dnsmasq"
+BLOCKLIST_TARGET_FILE_IPV6="/etc/hosts_ipv6.dnsmasq"
+
 # Download latest blocklist
-curl -k -o /etc/hosts.dnsmasq.new "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+echo "Fetching latest blocklist..."
+curl --progress -k -o $BLOCKLIST_TEMP_FILE $REMOTE_BLOCKLIST_URL
+echo "Received blocklist, $(grep "^0" $BLOCKLIST_TEMP_FILE | wc -l) entries."
 
 # Compare the remote blocklist to the local blocklist
-cmp -s /etc/hosts.dnsmasq.new /etc/hosts.dnsmasq
+cmp -s $BLOCKLIST_TEMP_FILE $BLOCKLIST_TARGET_FILE
 if [[ $? != 0 ]] ; then
+  echo "Local blocklist is out of date, updating local lists and restarting..."
+
   # Replace hosts file with the updated list
-  mv /etc/hosts.dnsmasq.new /etc/hosts.dnsmasq
+  mv $BLOCKLIST_TEMP_FILE $BLOCKLIST_TARGET_FILE
 
   # Convert ipv4 host blocking entries into additional ipv6 blocking entries
-  sort /etc/hosts.dnsmasq | uniq | grep "^0" | sed "s/0\.0\.0\.0/::/g" > /etc/hosts_ipv6.dnsmasq
+  sort $BLOCKLIST_TARGET_FILE | uniq | grep "^0" | sed "s/0\.0\.0\.0/::/g" > $BLOCKLIST_TARGET_FILE_IPV6
 
   # Kill the main process. Docker will bring back up the container when restart=always is set.
   pkill dnsmasq
+else
+  echo "Local blocklist is up to date."
 fi
